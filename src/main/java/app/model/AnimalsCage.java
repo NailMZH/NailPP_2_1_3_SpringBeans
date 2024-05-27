b578bd65887cf3257708e71aa9c03d0f631025c4
Nail <mazhitovnail@gmail.com>
2024-05-27 21:44:24 +0300

    Commit first

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,26 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("animalBean")
 public class AnimalsCage {
 
-    @Autowired
+    private Timer timer;
     private Animal animal;
+
+    @Autowired
+    public AnimalsCage(Timer timer, @Qualifier("dogBean") Animal animal) {
+        this.timer = timer;
+        this.animal = animal;
+    }
+
+    @Autowired
+    public Timer getTimer(){
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
