15ad4e642cb3734efbf6fd41ccca6032d3eb9897
Nail <mazhitovnail@gmail.com>
2024-05-28 13:23:29 +0300

    Commit third "Исправил метод whatAnimalSay, теперь время в методе main одно и тоже"

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -4,4 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component("dogBean")
 public class Dog extends Animal {
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
 }
